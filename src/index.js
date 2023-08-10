d1c651fc324d85e15776ef635292ca9f4449cddf
Alv0425 <vilejshikova@gmail.com>
2023-08-10 15:46:22 +0300

    feat: add code converting given number into reverse one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
   //start reverse int task
+  let strnum = Math.abs(n)+ '';
+  let reverseNum = '';
+  strnum.split('').reverse().forEach(digit => reverseNum += digit);
+  return reverseNum*1;
 }
